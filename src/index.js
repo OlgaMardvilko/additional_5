bb5749a86fa3f4ab7cf75924bbc10a39318b2d50
vitol <omardvilko@mail.ru>
2017-09-29 17:53:33 +0300

    Brackets my

--- src/index.js
+++ src/index.js
@@ -1,59 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
   
-	var arr = str.split(''); //массив со строками 
-	console.log(arr);
-  
-	var open = [], close = [];		
-		for (var i = 0; i < bracketsConfig.length; i++) {
-		open.push(bracketsConfig[i][0]);  	 //открывающие элементы
-		close.push(bracketsConfig[i][1]);		//закрывающие элементы
+	var pair = []; 		//один элемент массива = пара открывающего и закрывающего элемента из bracketsConfig
+	var indexPairStr = 0; 		//индекс найденной пары в строке
+
+	for (var i = 0; i < bracketsConfig.length; i++) { 	 // объединяем открывающий и закрывающий элемент = пара
+		var openClose = bracketsConfig[i][0] + bracketsConfig[i][1];
+		pair.push(openClose);
 	}
-	
-	var closeRes = [];
-	
-	if ((arr.length %2) != 0){  // все нечетные не будут иметь закрывающего эл.
-		return false;
-	}
-	
-	for(var i = 0; i<arr.length; i++){
-			for(var j = 0; j<open.length; j++){
-				if (arr[i]==open[j]){
-					closeRes.push(close[j]);
-				}	
+
+	function searchPairs() {  					// проверяем строку и вырезаем все пары  
+		for (var j = 0; j < pair.length; ) {
+			if (str.indexOf(pair[j]) !== -1) { 		//если найдет пару открывающего и закрывающего элемента
+				var indexPairStr = str.indexOf(pair[j]); 			//найденная пара в строке
+				str = str.substring(0, indexPairStr) + str.slice(indexPairStr+2); 	//вырезаем из строки найденную пару и возвращаем строку без нее
+				j = 0;													//опять проверяем сначала строки						
+			} 
+			else if (str.indexOf(pair[j]) === -1) {		 //если нет пары открывающих и закрывающих элементов 
+				j++;
 			}
-			if(arr[i+1] == closeRes[i]){  // нашли все закрывающие элементы для нашей строки
-						console.log(arr[i]);
-					
-						arr.splice(0, 2); // начиная с позиции 1, удалить 2 элемент
-					}
-		
-	}
-		
-	if (arr.length == 0){  // если в результате удаления парных в массиве не осталось элементов 
-		return true;
-	}
-	
-	var arrRevers = arr.reverse();     // перворачиваем результирующий массив
-		console.log(arrRevers);
-	
-	var nepara = 0;
-	
-	for(var i = 0; i<arrRevers.length; i++){
-		console.log(arrRevers[i]);
-		console.log(closeRes[i]);
-		
-		if(arrRevers[i]!== closeRes[i]){
-			nepara ++;
 		}
-		
-	}
-	console.log(nepara);
-	console.log(arrRevers.length);
-	if (nepara !== 0 && arrRevers.length == 2){
-		return false;
-	}
-	else{
-		return true;
 	}
   
+	searchPairs();
+  
+	if (str.length > 0) {  //если после проверки и вырезания пар остались элементы, то они не brackets
+		return false;
+	} 
+	else {
+		return true;
+	}
+
 }
